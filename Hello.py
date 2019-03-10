0c6cc532d70d256eec1168dbeacf9c02e0805189
Farahhasan <farah.hasan0105@gmail.com>
2019-03-10 23:44:55 +0530

    Message_code

--- Hello.py
+++ Hello.py
@@ -3,3 +3,7 @@ print("Hi Hasan")
 print("Good Morning ")
 print("Have a good day")
 print("Good Night")
+def wish_luck(anything):
+    thing= anything+ " Wish me luck"
+    return thing
+print(wish_luck("Happy"))
